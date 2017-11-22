97402cb8bcc9584bf26999f20a2fb9add86be250
fpampreen <fpampreen@aras.com>
2017-11-22 11:24:15 -0500

    Renamed addin
    Office Connector Extension Addin

--- WordAddIn1/Properties/AssemblyInfo.cs
+++ WordAddIn1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WordAddIn1")]
+[assembly: AssemblyTitle("Office Connector Extension Addin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WordAddIn1")]
+[assembly: AssemblyProduct("Office Connector Extension Addin")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
